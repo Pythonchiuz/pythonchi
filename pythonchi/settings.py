b6a7c1f8bad361cab6e3811d0e7b91723c351358
Pythonchiuz <143520238+Pythonchiuz@users.noreply.github.com>
2025-01-15 18:34:22 +0500

    final commit

--- pythonchi/settings.py
+++ pythonchi/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/pythonch/public_html/static'
+STATIC_ROOT = '/home/pythonc1/public_html/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/pythonch/public_html/media'
+MEDIA_ROOT = '/home/pythonc1/public_html/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
